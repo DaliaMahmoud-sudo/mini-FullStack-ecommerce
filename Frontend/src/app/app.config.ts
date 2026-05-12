1fcde687a90ce46e95548c554c022af16189305a
DaliaMahmoud-sudo <dalia.m3544214@gmail.com>
2026-05-12 13:21:28 +0300

    feat: add interceptors and landing page

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,16 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation, withInMemoryScrolling, withViewTransitions } from '@angular/router';
-
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './core/interceptors/auth-interceptor';
-
+import { errorInterceptor } from './core/interceptors/error-interceptor';
+import { NgxSpinnerModule } from "ngx-spinner";
+import { loadingInterceptor } from './core/interceptors/loading-interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' }), withViewTransitions(), withHashLocation()),
-    provideHttpClient(withFetch(),withInterceptors([authInterceptor]))
+    provideHttpClient(withFetch(),withInterceptors([errorInterceptor, loadingInterceptor])),
+     provideToastr(),
+     importProvidersFrom(NgxSpinnerModule)
 
   ]
 };
